47beb9075b620dd7a139f37ad6ab21492bf78639
Sibusiso94 <58597982+Sibusiso94@users.noreply.github.com>
2022-09-15 13:35:27 +0200

    Add SurfSpotTitleView

---
+++ UIViews/ReusbaleView/SurfSpotTitleView.swift
@@ -0,0 +1,20 @@
+//
+//  SurfSpotTitleView.swift
+//  UIViews
+//
+//  Created by Sibusiso Mbonani on 2022/09/15.
+//
+
+import UIKit
+
+class SurfSpotTitleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
