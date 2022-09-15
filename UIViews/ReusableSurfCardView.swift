8a85fac35cd83f44819c442882633647766ae8c6
Sibusiso94 <58597982+Sibusiso94@users.noreply.github.com>
2022-09-15 09:45:32 +0200

    Complete Xib design file

---
+++ UIViews/ReusableSurfCardView.swift
@@ -0,0 +1,15 @@
+//
+//  ReusableSurfCardView.swift
+//  UIViews
+//
+//  Created by Sibusiso Mbonani on 2022/09/15.
+//
+
+import UIKit
+
+class ReusableSurfCardView: UIView {
+
+    @IBOutlet weak var companyLogoImage: UIImageView!
+    
+
+}
